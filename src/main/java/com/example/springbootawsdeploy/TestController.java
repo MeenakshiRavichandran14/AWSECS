e9405722557c544cf5febdb4b6e7287250dc77ce
root <root@ip-172-31-14-84.ap-south-1.compute.internal>
2026-09-04 18:09:04 +0000

    third message

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from AWS Ecs";
+        return "Updated message from AWS Ecs";
     }
 
 }
